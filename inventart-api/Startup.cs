77cae95566b7450edc9a82fea0f46c688fccbf79
heldersousa <3x9equals27@gmail.com>
2021-04-03 23:46:08 +0200

    activated developer exceptions in release

--- inventart-api/Startup.cs
+++ inventart-api/Startup.cs
@@ -61,10 +61,10 @@ namespace Inventart
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+           // {
                 app.UseDeveloperExceptionPage();
-            }
+           // }
 
             app.UseSwagger();
             app.UseSwaggerUI(c => {
