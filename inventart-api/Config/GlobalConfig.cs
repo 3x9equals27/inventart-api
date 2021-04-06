3d2480c32dc54a3462798e3939eefa674be683c2
heldersousa <3x9equals27@gmail.com>
2021-04-06 23:04:27 +0200

    return link to file from file guid

---
+++ inventart-api/Config/GlobalConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventart.Config
+{
+    public class GlobalConfig
+    {
+        //Global static stuff
+        public const string WwwRootModelFolder = "models";
+        //read from settings
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ inventart-api/Controllers/FileController.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using Inventart.Config;
+using Inventart.Services.Singleton;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FileIO = System.IO.File;
+
+namespace Inventart.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+        private readonly IWebHostEnvironment _wenv;
+        private readonly ConnectionStringProvider _csp;
+
+        public FileController(
+            ILogger<FileController> logger, 
+            IWebHostEnvironment webHostEnvironment,
+            ConnectionStringProvider connectionStringProvider)
+        {
+            _logger = logger;
+            _wenv = webHostEnvironment;
+            _csp = connectionStringProvider;
+        }
+
+        [HttpGet("link")]
+        public async Task<IActionResult> GetLinkForFile(Guid fileGuid)
+        {
+            if(Guid.Empty == fileGuid) return BadRequest();
+
+            string folderPath = Path.Combine(_wenv.WebRootPath, GlobalConfig.WwwRootModelFolder, $"{fileGuid}");
+
+            if (Directory.Exists(folderPath))
+            {
+                string[] files = Directory.GetFiles(folderPath);
+                if (files.Length == 1)
+                {
+                    return Ok(LinkToFile(fileGuid, Path.GetFileName(files[0])));
+                }
+                else
+                {
+                    Directory.Delete(folderPath, true);
+                }
+            }
+            
+            //WIP: get this in its own service and use a stored proc
+            List<dynamic> results = new List<dynamic>();
+            var sql = "SELECT name, bytes FROM file WHERE guid = @guid";
+
+            using (var connection = new NpgsqlConnection(_csp.ConnectionString))
+            {
+                results = (await connection.QueryAsync(sql, new { guid = fileGuid })).ToList();
+            }
+
+            //return null when this is a function that has to return a object of type { name, bytes} and add this logic with a "is null" instead after the function returns
+            if (results.Count != 1) return NotFound();
+
+            byte[] fileBytes = results[0].bytes;
+            string name = results[0].name;
+            //
+
+            Directory.CreateDirectory(folderPath);
+            string filePath = Path.Combine(folderPath, name);
+            FileIO.WriteAllBytes(filePath, fileBytes);
+
+            return Ok(LinkToFile(fileGuid,name));
+        }
+
+        private string LinkToFile(Guid fileGuid, string fileName)
+        {
+            return $"{Request.Scheme}://{Request.Host.Value}/{GlobalConfig.WwwRootModelFolder}/{fileGuid}/{fileName}";
+        }
+
+    }
+}
